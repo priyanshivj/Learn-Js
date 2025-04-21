10e0e0f28eb4a783a4846a461b42745807d83084
Priyanshi Vijay <priyanshivijay100@gmail.com>
2025-04-21 16:01:08 +0530

    type conversion and coercion done

---
+++ 01-Fundamentals-Part-1/starter/boolean.js
@@ -0,0 +1,2 @@
+//5 falsy values : 0, "", undefined, null, NaN
+
---
+++ 01-Fundamentals-Part-1/starter/type-conv.js
@@ -0,0 +1,26 @@
+// type conversion
+
+// const inputYear = '1999';
+// console.log(Number(inputYear) + 18); //2017
+// console.log(inputYear + 18); // 199918
+// console.log(Number(inputYear), inputYear); // 1999 '1999'
+
+// console.log(Number("smiley"));
+// console.log(typeof NaN);
+
+// console.log(String(23), 25);
+
+// type coercion
+
+console.log('I am ' + 23 + ' years old');
+console.log(`I am ${25} years old!`);
+
+console.log('23' - '10' - 2); //11
+console.log('23' + '10' + 2);//23102
+console.log('23' * '2');
+console.log('23' / '2');
+
+let n = '1' + 1; //11
+n = n - 1;  //11 - 1 = 10
+console.log(n); //10
+
