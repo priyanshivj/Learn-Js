46404680119a9193c1a9edfda667e969b5e1a5cd
Priyanshi Vijay <priyanshivijay100@gmail.com>
2025-04-21 16:28:37 +0530

    boolean done true and falsy value

--- 01-Fundamentals-Part-1/starter/boolean.js
+++ 01-Fundamentals-Part-1/starter/boolean.js
@@ -1,2 +1,28 @@
 //5 falsy values : 0, "", undefined, null, NaN
+console.log(Boolean(0));
+console.log((Boolean));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+console.log(Boolean(""));
+console.log(Boolean("Smiley"));//true
+console.log(Boolean({})); // true
+
+
+
+const money = 100;
+
+if (money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log("You should get a job!");
+}
+
+let height = 0;
+if (height) {
+    console.log('Yay! Height is defined');
+} else {
+    console.log('Height is UNDEFINED!');
+}
+
 
